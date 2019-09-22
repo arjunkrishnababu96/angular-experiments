5282972b4a1e0d1d0979960abbcefcb9072bc37a
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-09-22 15:22:34 +0530

    update AuthService to (pretend) log in the user, and revise AuthGuard to make use of the service

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
       console.log("AuthGuard#canActivate called!");
-      return true;
+
+      let url: string = state.url;
+
+      return this.checkLogin(url);
   }
-  
+
+  checkLogin(url: string): boolean {
+    // throw new Error("Method not implemented.");
+    if(this.authService.isLoggedIn) {
+      return true;
+    }
+
+    // store the attempted URL for redirecting
+    this.authService.redirectUrl = url;
+    this.router.navigate(['/login']);
+    return false;
+  }
 }
